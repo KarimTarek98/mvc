ba3208ddfe41ca76cf5cadaedb7f9318bc12187b
Karim Tarek <karimabougom3@gmail.com>
2018-09-06 04:51:37 +0200

    Load template in not found action instead of h1 message

--- xampp/htdocs/mvc/app/controllers/notfoundcontroller.php
+++ xampp/htdocs/mvc/app/controllers/notfoundcontroller.php
@@ -4,5 +4,10 @@ namespace PHPMVC\Controllers;
 
 class NotFoundController extends AbstractController
 {
-
+    public function defaultAction()
+    {
+        $this->_language->load('template.common');
+//        $this->_language->load('index.default');
+        $this->_view();
+    }
 }
